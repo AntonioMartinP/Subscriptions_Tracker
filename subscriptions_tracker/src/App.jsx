2d4f0aa2ee578c088cc1b9346a3166a02de0c754
AntonioMartinP <josoner371@exitbit.com>
2025-07-17 05:16:08 +0200

    First stage

---
+++ subscriptions_tracker/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import FormAddMoney from './components/FormAddMoney'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="App">
+        <Header />
+        <FormAddMoney />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ subscriptions_tracker/src/components/FormAddMoney.jsx
@@ -0,0 +1,12 @@
+const FormAddMoney = () => {
+  return (
+    <div className="form-add-money">
+      <h2>Add Money</h2>
+      <form>
+        <input type="number" placeholder="Amount" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  )
+}
+export default FormAddMoney;
